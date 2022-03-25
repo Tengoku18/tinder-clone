cfdd3e708d22f42cc53a4290518a1b108c1943c7
Tengoku18 <tengokusenpai18@gmail.com>
2022-03-25 22:12:18 +0545

    comment

--- pages/components/SwipeButtons.js
+++ pages/components/SwipeButtons.js
@@ -10,6 +10,7 @@ import {
 function SwipeButtons() {
   return (
     <div className="flex justify-around mt-20 mx-20">
+      {/* this is flfkf */}
       <RefreshIcon className="text-yellow-300 bg-slate-100 shadow-md rounded-full px-2 py-1 h-12 w-12" />
 
       <XIcon className="text-gray-800 bg-slate-100 shadow-md rounded-full px-2 py-1 h-12 w-12" />
